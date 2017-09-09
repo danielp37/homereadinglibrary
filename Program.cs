f7e75ce02c77ae0a99636be3dd105515a5cc7100
Dan Preece <dan.preece37@gmail.com>
2017-09-08 22:26:03 -0600

    Upgrade to .net core 2.0

--- Program.cs
+++ Program.cs
@@ -4,7 +4,9 @@ using System.IO;
 using System.Linq;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Hosting.Internal;
 
 namespace WebApplicationBasic
 {
@@ -13,14 +15,12 @@ namespace WebApplicationBasic
         public static HttpClient HttpClient = new HttpClient();
         public static void Main(string[] args)
         {
-            var host = new WebHostBuilder()
-                .UseKestrel()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseIISIntegration()
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
                 .Build();
-
-            host.Run();
-        }
     }
 }
