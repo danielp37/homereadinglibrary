a2d028d5e90b72829999ee2ebdff3aeb19b3c8aa
Dan Preece <dan.preece37@gmail.com>
2018-09-13 19:48:49 -0600

    Added application insights

--- HomeReadingLibraryWeb/Program.cs
+++ HomeReadingLibraryWeb/Program.cs
@@ -19,6 +19,7 @@ namespace HomeReadingLibraryWeb
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
