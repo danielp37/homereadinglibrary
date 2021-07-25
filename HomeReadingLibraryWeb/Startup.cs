6f208c252a17c4f09591c7d786f77e536858dbd9
Dan Preece <dan.preece37@gmail.com>
2021-07-24 22:53:35 -0600

    Upgrade to net 5

--- HomeReadingLibraryWeb/Startup.cs
+++ HomeReadingLibraryWeb/Startup.cs
@@ -5,15 +5,10 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.SpaServices.AngularCli;
+using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.IdentityModel.Tokens;
-using System.Collections.Generic;
 using System.IO;
 using System.Security.Cryptography.X509Certificates;
 
@@ -31,8 +26,11 @@ namespace HomeReadingLibraryWeb
     // This method gets called by the runtime. Use this method to add services to the container.
     public void ConfigureServices(IServiceCollection services)
     {
-      services.AddMvc()
-        .SetCompatibilityVersion(CompatibilityVersion.Version_2_2)
+      services.AddMvc(opt =>
+      {
+        opt.EnableEndpointRouting = false;
+      })
+        .SetCompatibilityVersion(CompatibilityVersion.Version_3_0)
         .AddApplicationPart(typeof(HomeReadingLibrary.Controllers.ComponentRegistration).Assembly);
 
       // In production, the Angular files will be served from this directory
@@ -93,7 +91,7 @@ namespace HomeReadingLibraryWeb
     }
 
     // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-    public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
     {
       if (env.IsDevelopment())
       {
