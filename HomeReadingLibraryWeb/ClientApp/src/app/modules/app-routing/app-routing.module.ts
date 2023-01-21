f9e6a60f10c8c1c04f4f72d7083d333872d419b5
Dan Preece <dan.preece37@gmail.com>
2023-01-21 15:40:36 -0700

    Update to Angular 15

--- HomeReadingLibraryWeb/ClientApp/src/app/modules/app-routing/app-routing.module.ts
+++ HomeReadingLibraryWeb/ClientApp/src/app/modules/app-routing/app-routing.module.ts
@@ -41,7 +41,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' }),
+    RouterModule.forRoot(routes, {}),
     //AuthModule
    ],
   exports: [ RouterModule ]
